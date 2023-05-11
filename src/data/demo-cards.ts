1d63d44d8b34c6d71435bff0a026b29248d5025e
im-mahadi <me.mahadi@hotmail.com>
2023-05-11 18:22:21 +0600

    add demo card data

---
+++ src/data/demo-cards.ts
@@ -0,0 +1,34 @@
+import { ICard } from "@/interfaces/ICard";
+
+export const cards: ICard[] = [
+    {
+        id: 1,
+        title: "This is a card",
+        tags: ["card", "demo"],
+        time: new Date(),
+    },
+    {
+        id: 2,
+        title: "This is another card",
+        tags: ["card", "demo", "example"],
+        time: new Date(),
+    },
+    {
+        id: 3,
+        title: "This is a third card",
+        tags: ["card", "demo", "tutorial"],
+        time: new Date(),
+    },
+    {
+        id: 4,
+        title: "This is a fourth card",
+        tags: ["card", "demo", "question"],
+        time: new Date(),
+    },
+    {
+        id: 5,
+        title: "This is a fifth card",
+        tags: ["card", "demo", "answer"],
+        time: new Date(),
+    },
+];
